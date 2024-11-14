7149ec9972c91ae79ca12aea6d253bc67e0016f7
adib <76139191+adibabdala123@users.noreply.github.com>
2024-11-14 14:06:36 -0300

    Create spider.py

---
+++ spider.py
@@ -0,0 +1,12 @@
+import scrapy
+
+class ExampleSpider(scrapy.Spider):
+    name = "example"
+    start_urls = ['http://example.com/']
+
+    def parse(self, response):
+        for link in response.css('a::attr(href)').getall():
+            if any(link.endswith(ext) for ext in self.interesting_extensions):
+                yield {"file": link}
+            elif not link.startswith("#") and not link.startswith("mailto:"):
+                yield response.follow(link, callback=self.parse)
